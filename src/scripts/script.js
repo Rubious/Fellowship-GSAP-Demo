3343f60d996bc3245939139411fd43b9e62ef0b7
David <dashcroft@outlook.com>
2024-08-20 12:09:08 +0100

    update: chain the morph into the next one

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,7 +6,18 @@ import { MorphSVGPlugin } from "../../node_modules/gsap/MorphSVGPlugin.js";
 
 gsap.registerPlugin(ScrollSmoother,MorphSVGPlugin);
 
-var morph = gsap.to("#line", { duration: 4, ease: "power1.inOut", morphSVG: "#curve", repeat:1, yoyo:true, repeatDelay:0.2 });
+var morph = gsap.to("#line", { 
+    duration: 3, 
+    ease: "power1.inOut", 
+    morphSVG: "#curve",
+    onComplete: function(){
+        gsap.to("#line", { 
+            duration: 3, 
+            ease: "power1.inOut", 
+            morphSVG: "#curve-2"
+        });
+    }
+});
 
 document.getElementById("play").onclick = function(){
     morph.restart(true);
