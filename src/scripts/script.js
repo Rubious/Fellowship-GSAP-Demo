0bb1f92f2eb83577a5105a34b9be8a7cd7980c20
David <dashcroft@outlook.com>
2024-08-22 15:21:44 +0100

    refactor: break into modules

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,6 +3,8 @@ import { ScrollSmoother } from "gsap/dist/ScrollSmoother";
 import { MorphSVGPlugin } from "gsap/dist/MorphSVGPlugin";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
+import { line } from "./modules/line.js";
+
 /*/!* The following plugins are Club GSAP perks *!/
 import { ScrollSmoother } from "../../node_modules/gsap/ScrollSmoother.js";
 import { MorphSVGPlugin } from "../../node_modules/gsap/MorphSVGPlugin.js";
@@ -12,58 +14,6 @@ gsap.registerPlugin(ScrollSmoother,MorphSVGPlugin,ScrollTrigger);
 
 addEventListener("DOMContentLoaded", (event) => {
 
-    // create the scrollSmoother before your scrollTriggers
-    ScrollSmoother.create({
-        smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-        effects: true, // looks for data-speed and data-lag attributes on elements
-        smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-    });
-
-    gsap.to("#line", {  
-        morphSVG: "#squiggle-1",
-        duration: 1,
-        scrollTrigger: {
-            trigger: "#section-1",
-            scrub: true,
-            start: 'top top',
-            pin:"#section-1",
-        }
-    });
-
-    gsap.to('#line', {
-        morphSVG: "#squiggle-2",
-        duration: 1,
-        immediateRender:false,
-        scrollTrigger: {
-            trigger: "#section-2",
-            start: 'top top',
-            scrub: true,
-            pin:"#section-2",
-        }
-    });
-
-    gsap.to("#line", {
-        morphSVG: "#squiggle-3",
-        duration: 1,
-        immediateRender:false,
-        scrollTrigger: {
-            trigger: "#section-3",
-            start: 'top top',
-            scrub: true,
-            pin:"#section-3",
-        }
-    });
-
-    gsap.to("#line", {
-        morphSVG: "#line",
-        duration: 1,
-        immediateRender:false,
-        scrollTrigger: {
-            trigger: "#section-4",
-            start: 'top top',
-            scrub: true,
-            pin:"#section-4",
-        }
-    });
+    line(); //build line
 
 });
